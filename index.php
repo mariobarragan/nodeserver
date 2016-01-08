de066d6dd4fda10a2e4243747b8243066d3353d8
mariobarragan <perezluigi94@gmail.com>
2016-01-08 11:37:43 -0800

    Added my own index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$_SESSION['pagename'] = "juliett index";
+include "db/pushhits.php";
+?>
+
+<html>
+<head>
+<title> emptyness</title>
+</head>
+<body>
+
+<h1>you see nothing</h1>
+<p>stuff!</p>
+
+</body>
+</html>
+
